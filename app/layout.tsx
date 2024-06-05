1eab3819f1fd61fea3df68aecbca8b45b051e562
John Paul <iamjohnpaulr5@gmail.com>
2024-06-05 11:14:12 +0530

    chore: Add @vercel/analytics package and integrate it into the layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Toaster />
+        <Analytics/>
       </body>
     </html>
   );
